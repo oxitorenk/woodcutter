4d250099c2e78b8425c9fb5182d7be0cc898f841
Oğuzhan Akbulut <akbulutoguzhan@outlook.com.tr>
2021-08-14 00:21:28 +0300

    Feat: add player handler

---
+++ Assets/Scripts/PlayerHandler.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class PlayerHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject playerObject;
+
+    public Position PlayerPosition { get; private set; }
+
+    public enum Position
+    {
+        Left,
+        Right
+    }
+    
+    private void Start()
+    {
+        GameEvents.OnCutTheLog += PlayCutAnimation;
+
+        PlayerPosition = Position.Right;
+    }
+
+    public void MoveToRight()
+    {
+        if (PlayerPosition == Position.Right) return;
+        
+        PlayerPosition = Position.Right;
+        ChangeRotation(0);
+        ChangePosition();
+    }
+
+    public void MoveToLeft()
+    {
+        if (PlayerPosition == Position.Left) return;
+        
+        PlayerPosition = Position.Left;
+        ChangeRotation(180);
+        ChangePosition();
+    }
+
+    private void ChangePosition()
+    {
+        var position = playerObject.transform.position;
+        
+        playerObject.transform.position = new Vector3(position.x * -1, position.y);
+    }
+
+    private void ChangeRotation(float rotationY)
+    {
+        var rotation = playerObject.transform.rotation;
+
+        playerObject.transform.rotation = new Quaternion(rotation.x, rotationY, rotation.z, rotation.w);
+    }
+
+    private void PlayCutAnimation()
+    {
+        // Play
+    }
+}
