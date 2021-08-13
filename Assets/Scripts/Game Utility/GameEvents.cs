bee40341b38f81310e7ad2679b85ce5626e0dff2
Oğuzhan Akbulut <akbulutoguzhan@outlook.com.tr>
2021-08-14 00:21:39 +0300

    Feat: add game events

---
+++ Assets/Scripts/Game Utility/GameEvents.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameEvents
+{
+    public delegate void IncreaseDifficulty(int increaseValue);
+    public static event IncreaseDifficulty OnIncreaseDifficulty;
+    
+    public static void IncreaseDifficultyMethod(int increaseValue)
+    {
+        OnIncreaseDifficulty?.Invoke(increaseValue);
+    }
+
+    public delegate void DecreaseDifficulty(int decreaseValue);
+    public static event DecreaseDifficulty OnDecreaseDifficulty;
+
+    public static void DecreaseDifficultyMethod(int decreaseValue)
+    {
+        OnDecreaseDifficulty?.Invoke(decreaseValue);
+    }
+
+    public delegate void CutTheLog();
+    public static event CutTheLog OnCutTheLog;
+
+    public static void CutTheLogMethod()
+    {
+        OnCutTheLog?.Invoke();
+    }
+
+    public delegate void GameOver();
+    public static event GameOver OnGameOver;
+
+    public static void GameOverMethod()
+    {
+        OnGameOver?.Invoke();
+    }
+}
