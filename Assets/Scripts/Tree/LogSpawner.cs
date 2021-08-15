0166313ff256256841bb77dcc6714662ec3f56d4
Oğuzhan Akbulut <akbulutoguzhan@outlook.com.tr>
2021-08-16 01:40:02 +0300

    Feat(Log-Spawn): Improve branch spawn chance & get tag names via variables

--- Assets/Scripts/Tree/LogSpawner.cs
+++ Assets/Scripts/Tree/LogSpawner.cs
@@ -1,70 +1,74 @@
-using System;
+using Game_Utility;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class LogSpawner : MonoBehaviour
+namespace Tree
 {
-    [SerializeField] private GameObject normalLogPrefab;
-    [SerializeField] private GameObject leftBranchLogPrefab;
-    [SerializeField] private GameObject rightBranchLogPrefab;
-
-    public string LeftBranchTag { get; private set; }
-    public string RightBranchTag { get; private set; }
-    
-    private const int MaxBranchChance = 80;
-    private const int MinBranchChance = 40;
-    private const int BranchChanceIncreaseValue = 10;
-    private int _branchChance;
-
-    private void Start()
+    public class LogSpawner : MonoBehaviour
     {
-        GameEvents.OnIncreaseDifficulty += IncreaseBranchChance;
-        GameEvents.OnDecreaseDifficulty += DecreaseBranchChange;
+        [SerializeField] private GameObject normalLogPrefab;
+        [SerializeField] private GameObject leftBranchLogPrefab;
+        [SerializeField] private GameObject rightBranchLogPrefab;
 
-        LeftBranchTag = leftBranchLogPrefab.tag;
-        RightBranchTag = rightBranchLogPrefab.tag;
+        public string LeftBranchTag { get; private set; }
+        public string RightBranchTag { get; private set; }
 
-        _branchChance = MinBranchChance;
-    }
+        private const int MaxBranchChance = 80;
+        private const int MinBranchChance = 40;
+        private const int BranchChanceIncreaseValue = 10;
+        private int _branchChance;
 
-    private void OnDestroy()
-    {
-        GameEvents.OnIncreaseDifficulty -= IncreaseBranchChance;
-        GameEvents.OnDecreaseDifficulty -= DecreaseBranchChange;
-    }
-
-    public GameObject GetNormalLog()
-    {
-        return normalLogPrefab;
-    }
-    public GameObject GetRandomLog(string logTag)
-    {
-        var randomValue = Random.Range(1, 101);
-
-        if (randomValue <= _branchChance)
+        private void Start()
         {
-            var randomBranch = Random.Range(0, 2);
-            
-            return logTag switch
-            {
-                "Left" => randomBranch == 0 ? leftBranchLogPrefab : normalLogPrefab,
-                "Right" => randomBranch == 0 ? rightBranchLogPrefab : normalLogPrefab,
-                _ => randomBranch == 0 ? leftBranchLogPrefab : rightBranchLogPrefab
-            };
+            GameEvents.OnIncreaseDifficulty += IncreaseBranchChance;
+            GameEvents.OnDecreaseDifficulty += DecreaseBranchChange;
+
+            LeftBranchTag = leftBranchLogPrefab.tag;
+            RightBranchTag = rightBranchLogPrefab.tag;
+
+            _branchChance = MinBranchChance;
         }
 
-        return normalLogPrefab;
-    }
-    
-    private void IncreaseBranchChance()
-    {
-        if (_branchChance < MaxBranchChance) 
-            _branchChance += BranchChanceIncreaseValue;
-    }
+        private void OnDestroy()
+        {
+            GameEvents.OnIncreaseDifficulty -= IncreaseBranchChance;
+            GameEvents.OnDecreaseDifficulty -= DecreaseBranchChange;
+        }
 
-    private void DecreaseBranchChange()
-    {
-        if (_branchChance > MinBranchChance)
-            _branchChance -= BranchChanceIncreaseValue;
+        public GameObject GetNormalLog()
+        {
+            return normalLogPrefab;
+        }
+
+        public GameObject GetRandomLog(string logTag)
+        {
+            var randomValue = Random.Range(1, 101);
+
+            if (randomValue <= _branchChance)
+            {
+                var randomBranch = Random.Range(0, 2);
+
+                return logTag switch
+                {
+                    "Left" => randomBranch == 0 ? leftBranchLogPrefab : normalLogPrefab,
+                    "Right" => randomBranch == 0 ? rightBranchLogPrefab : normalLogPrefab,
+                    _ => randomBranch == 0 ? leftBranchLogPrefab : rightBranchLogPrefab
+                };
+            }
+
+            return normalLogPrefab;
+        }
+
+        private void IncreaseBranchChance()
+        {
+            if (_branchChance < MaxBranchChance)
+                _branchChance += BranchChanceIncreaseValue;
+        }
+
+        private void DecreaseBranchChange()
+        {
+            if (_branchChance > MinBranchChance)
+                _branchChance -= BranchChanceIncreaseValue;
+        }
     }
 }
