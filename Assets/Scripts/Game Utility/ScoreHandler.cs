da11f983635a7a7301bfff39e0464e769fec51b0
Oğuzhan Akbulut <akbulutoguzhan@outlook.com.tr>
2021-08-16 01:36:58 +0300

    Refactor(Score): Add namespace to score handler

--- Assets/Scripts/Game Utility/ScoreHandler.cs
+++ Assets/Scripts/Game Utility/ScoreHandler.cs
@@ -2,71 +2,74 @@ using System;
 using System.IO;
 using UnityEngine;
 
-public class ScoreHandler : MonoBehaviour
+namespace Game_Utility
 {
-    private static string _dataPath;
-
-    private DifficultyHandler _difficultyHandler;
-
-    public int CurrentScore { get; private set; }
-
-    private void Start()
+    public class ScoreHandler : MonoBehaviour
     {
-        _difficultyHandler = gameObject.GetComponent<DifficultyHandler>();
-        
-        GameEvents.OnCutTheLog += IncreaseScore;
-        GameEvents.OnGameOver += SaveScore;
-        
-        _dataPath = Application.persistentDataPath + "ScoreData.json";
+        private static string _dataPath;
 
-        if (!File.Exists(_dataPath))
+        private DifficultyHandler _difficultyHandler;
+
+        public int CurrentScore { get; private set; }
+
+        private void Start()
         {
-            InitializeFirstTime();
+            _difficultyHandler = gameObject.GetComponent<DifficultyHandler>();
+        
+            GameEvents.OnCutTheLog += IncreaseScore;
+            GameEvents.OnGameOver += SaveScore;
+        
+            _dataPath = Application.persistentDataPath + "ScoreData.json";
+
+            if (!File.Exists(_dataPath))
+            {
+                InitializeFirstTime();
+            }
         }
-    }
 
-    private void OnDestroy()
-    {
-        GameEvents.OnCutTheLog -= IncreaseScore;
-        GameEvents.OnGameOver -= SaveScore;
-    }
+        private void OnDestroy()
+        {
+            GameEvents.OnCutTheLog -= IncreaseScore;
+            GameEvents.OnGameOver -= SaveScore;
+        }
 
-    private void IncreaseScore()
-    {
-        CurrentScore += _difficultyHandler.Level;
-    }
+        private void IncreaseScore()
+        {
+            CurrentScore += _difficultyHandler.Level;
+        }
     
-    private void SaveScore()
-    {
-        var highScore = GetHighScore();
+        private void SaveScore()
+        {
+            var highScore = GetHighScore();
 
-        if (CurrentScore < highScore) return;
+            if (CurrentScore < highScore) return;
 
-        var scoreData = new Score {score = CurrentScore};
-        var scoreJson = JsonUtility.ToJson(scoreData);
+            var scoreData = new Score {score = CurrentScore};
+            var scoreJson = JsonUtility.ToJson(scoreData);
         
-        File.WriteAllText(_dataPath, scoreJson);
-    }
+            File.WriteAllText(_dataPath, scoreJson);
+        }
     
-    public static int GetHighScore()
-    {
-        var scoreJson = File.ReadAllText(_dataPath);
-        var scoreData = JsonUtility.FromJson<Score>(scoreJson);
+        public static int GetHighScore()
+        {
+            var scoreJson = File.ReadAllText(_dataPath);
+            var scoreData = JsonUtility.FromJson<Score>(scoreJson);
 
-        return scoreData.score;
-    }
+            return scoreData.score;
+        }
 
-    private static void InitializeFirstTime()
-    {
-        var scoreData = new Score {score = 0};
-        var scoreJson = JsonUtility.ToJson(scoreData);
+        private static void InitializeFirstTime()
+        {
+            var scoreData = new Score {score = 0};
+            var scoreJson = JsonUtility.ToJson(scoreData);
         
-        File.WriteAllText(_dataPath, scoreJson);
-    }
+            File.WriteAllText(_dataPath, scoreJson);
+        }
 
-    [Serializable]
-    public struct Score
-    {
-        public int score;
+        [Serializable]
+        public struct Score
+        {
+            public int score;
+        }
     }
 }
