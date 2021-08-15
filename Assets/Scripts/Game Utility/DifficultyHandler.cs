5526e30a11f8fabb5f98f09a4a5b411fe30287f5
Oğuzhan Akbulut <akbulutoguzhan@outlook.com.tr>
2021-08-16 01:33:29 +0300

    Feat(Difficulty): Improve difficulty logic

--- Assets/Scripts/Game Utility/DifficultyHandler.cs
+++ Assets/Scripts/Game Utility/DifficultyHandler.cs
@@ -1,91 +1,92 @@
-using System;
 using UnityEngine;
 
-public class DifficultyHandler : MonoBehaviour
+namespace Game_Utility
 {
-    public int Level { get; private set; }
-    public float ProgressBar { get; private set; }
-    
-    private const int ProgressValue = 2;
-    private const float IncreaseValue = 0.015f;
-    private float _threshold = 0.37f;
-    private float _passedTime;
-    private bool _isGameStarted;
-    
-    private void Start()
+    public class DifficultyHandler : MonoBehaviour
     {
-        GameEvents.OnCutTheLog += MakeProgress;
-        GameEvents.OnStartGame += StartGame;
-        
-        ProgressBar = 50;
-        IncreaseDifficulty();
-    }
+        public int Level { get; private set; }
+        public float ProgressBar { get; private set; }
 
-    private void OnDestroy()
-    {
-        GameEvents.OnCutTheLog -= MakeProgress;
-        GameEvents.OnStartGame -= StartGame;
-    }
+        private const int ProgressValue = 2;
+        private const float IncreaseValue = 0.015f;
+        private float _threshold = 0.34f;
+        private float _passedTime;
+        private bool _isGameStarted;
 
-    private void Update()
-    {
-        CheckProgress();
-    }
-
-    private void StartGame()
-    {
-        _isGameStarted = true;
-    }
-
-    private void MakeProgress()
-    {
-        if (!_isGameStarted)
-            GameEvents.StartGameMethod();
-        
-        ProgressBar += ProgressValue;
-    }
-
-    private void IncreaseDifficulty()
-    {
-        Level++;
-        _threshold -= IncreaseValue;
-
-        GameEvents.IncreaseDifficultyMethod();
-    }
-
-    private void DecreaseDifficulty()
-    {
-        Level--;
-        _threshold += IncreaseValue;
-
-        if (Level < 1)
+        private void Start()
         {
-            GameEvents.GameOverMethod();
-        }
+            GameEvents.OnStartGame += StartGame;
+            GameEvents.OnCutTheLog += MakeProgress;
 
-        GameEvents.DecreaseDifficultyMethod();
-    }
-
-    private void CheckProgress()
-    {
-        if (_isGameStarted) 
-            _passedTime += Time.deltaTime;
-
-        if (_passedTime >= _threshold)
-        {
-            ProgressBar -= ProgressValue;
-            _passedTime -= _threshold;
-        }
-        
-        if (ProgressBar >= 100)
-        {
+            ProgressBar = 50;
             IncreaseDifficulty();
-            ProgressBar = 30;
         }
-        else if (ProgressBar <= 0)
+
+        private void OnDestroy()
         {
-            DecreaseDifficulty();
-            ProgressBar = 70;
+            GameEvents.OnStartGame -= StartGame;
+            GameEvents.OnCutTheLog -= MakeProgress;
+        }
+
+        private void Update()
+        {
+            CheckProgress();
+        }
+
+        private void StartGame()
+        {
+            _isGameStarted = true;
+        }
+
+        private void MakeProgress()
+        {
+            if (!_isGameStarted) return;
+
+            ProgressBar += ProgressValue;
+        }
+
+        private void CheckProgress()
+        {
+            if (_isGameStarted)
+                _passedTime += Time.deltaTime;
+
+            if (_passedTime >= _threshold)
+            {
+                ProgressBar -= ProgressValue;
+                _passedTime -= _threshold;
+            }
+
+            if (ProgressBar >= 100)
+            {
+                IncreaseDifficulty();
+                ProgressBar = 30;
+            }
+            else if (ProgressBar <= 0)
+            {
+                DecreaseDifficulty();
+                ProgressBar = 70;
+            }
+        }
+
+        private void IncreaseDifficulty()
+        {
+            Level++;
+            _threshold -= IncreaseValue;
+
+            GameEvents.IncreaseDifficultyMethod();
+        }
+
+        private void DecreaseDifficulty()
+        {
+            Level--;
+            _threshold += IncreaseValue;
+
+            if (Level < 1)
+            {
+                GameEvents.GameOverMethod();
+            }
+
+            GameEvents.DecreaseDifficultyMethod();
         }
     }
 }
